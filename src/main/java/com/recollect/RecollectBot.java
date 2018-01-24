c8fe76963862407df629d50dd4e8ea01d8684795
Vlad <vkiro@yandex.ua>
2018-01-24 21:02:45 +0200

    Changed code to save message/user in DB and to send user`s notes (#13).

--- src/main/java/com/recollect/RecollectBot.java
+++ src/main/java/com/recollect/RecollectBot.java
@@ -1,16 +1,20 @@
 package com.recollect;
 
+import com.recollect.dao.NoteDAO;
+import com.recollect.dao.UserDAO;
+import com.recollect.domain.Note;
+import com.recollect.domain.User;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
-import java.io.File;
+import java.util.Date;
+import java.util.List;
 
 public class RecollectBot extends TelegramLongPollingBot {
 
@@ -31,24 +35,40 @@ public class RecollectBot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
-        sendMessage(message);
+        if (message.isCommand()) {
+            List<Note> notes = NoteDAO.INSTANCE.getAllByUserId(message.getFrom().getId());
+            sendListMessages(notes, message);
+        } else {
+            saveMessage(message);
+        }
     }
 
-    public void sendMessage(Message message) {
-        File file = new File("C:/image.jpg");
+    private void sendListMessages(List<Note> notes, Message message) {
+        notes.forEach(note->sendMessage(note, message));
+    }
+
+    private void sendMessage(Note note, Message message) {
         SendMessage sendMessage = new SendMessage();
-        SendPhoto sendPhoto = new SendPhoto();
-        sendPhoto.setChatId(message.getChatId());
-        sendPhoto.setNewPhoto(file);
         sendMessage.setChatId(message.getChatId());
-        String s = "Hello, " + message.getFrom().getFirstName() + '\n' + message.getFrom().getLastName();
-        sendMessage.setText(s);
+        sendMessage.setText(note.getText());
         try {
+            //TODO change to not deprecated
             sendMessage(sendMessage);
-            sendPhoto(sendPhoto);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    private void saveMessage(Message message) {
+        //TODO change constructor and new user save in DB - old user continue
+        User user = new User(new Long(message.getFrom().getId()), message.getFrom().getFirstName(), message.getFrom().getBot(), message.getFrom().getLastName(), message.getFrom().getLanguageCode(), message.getFrom().getLanguageCode());
+        try {
+            UserDAO.INSTANCE.create(user);
+        } catch (Exception e) {
+            System.out.println("This user is already exists");
+        }
+        Note note = new Note(message.getText(), new Date(), user);
+        NoteDAO.INSTANCE.create(note);
     }
 
     public String getBotUsername() {
