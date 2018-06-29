690e8872ea115774d2b5d41d26ec81d3770e2ef3
Vlad <vkiro@yandex.ua>
2018-06-29 15:38:20 +0300

    Updated JUnit NoteDAO test (#18).

--- src/test/java/com/recollect/dao/NoteDAOTest.java
+++ src/test/java/com/recollect/dao/NoteDAOTest.java
@@ -1,39 +1,11 @@
 package com.recollect.dao;
 
-import static org.junit.Assert.*;
-
-import com.recollect.domain.Note;
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.util.Calendar;
 import org.junit.Test;
 
 public class NoteDAOTest {
 
   @Test
-  public void getFirstOrderByDateNotSend() throws Exception {
+  public void getFirstOrderByDateNotSend() {
     NoteDAO.INSTANCE.getFirstOrderByDateNotSend();
   }
-
-  @Test
-  public void create() throws Exception {
-    Note note = new Note();
-    note.setChatId(333899786L);
-    note.setText("BlaBlaCar");
-    note.setSent(false);
-    note.setDate(new Timestamp(System.currentTimeMillis()));
-    NoteDAO.INSTANCE.create(note);
-  }
-
-  @Test
-  public void update() throws Exception {
-    Note note = new Note();
-    note.setId(61L);
-    note.setChatId(333899786L);
-    note.setText("AAAAAAAAAAAAaaaaaaaaaaaaaa");
-    note.setSent(false);
-    note.setDate(new Timestamp(System.currentTimeMillis()));
-    NoteDAO.INSTANCE.update(note);
-  }
-
 }
