361ee2c20afd30ab3895b0620d32504186452839
Vlad <vkiro@yandex.ua>
2018-08-14 18:00:30 +0300

    Refactored.

--- src/main/java/com/recollect/service/TimeZoneTracker.java
+++ src/main/java/com/recollect/service/TimeZoneTracker.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TimeZoneTracker {
+  public static int UKRAINE_TIME_ZONE = 3;
 
   // TODO add more languageCodes and timezone hours
   private static final Map<String, Integer> timeZone = new HashMap<String, Integer>() {{
-    put("uk-UA", 3);
+    put("uk-UA", UKRAINE_TIME_ZONE);
   }};
 
   public static int getHours(String languageCode){
